28c0f2ea0b562572ef28289daf0a97e9c57af0dc
Mike Brooks <35354129+PDXmjb@users.noreply.github.com>
2022-12-17 10:26:43 -0800

    (fix) Policy Holder
    Fixing posting/fetching of data

--- src/components/PolicyHoldersView/PolicyHoldersView.tsx
+++ src/components/PolicyHoldersView/PolicyHoldersView.tsx
@@ -1,13 +1,93 @@
 import { Box } from '@mui/material';
+import Button from '@mui/material/Button';
 import { useQuery } from 'react-query';
 import InfoTable from '../InfoTable';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+interface PolicyHolder {
+  name: string;
+  age: string;
+  phoneNumber: string;
+  isPrimary: boolean;
+  address: {
+    line1: string;
+    line2: string;
+    city: string;
+    state: string;
+    postalCode: string;
+  };
+}
+
+interface Row {
+  key: string;
+  value: string;
+}
+
+function getPolicyHolderKeys(policyHolders: PolicyHolder[]): Row[][] {
+  return policyHolders.map((policyHolder: PolicyHolder) => {
+    const { name, age, phoneNumber, isPrimary, address } = policyHolder;
+
+    return [
+      { key: 'Name', value: name },
+      { key: 'Age', value: age },
+      {
+        key: 'Address',
+        value: address.line2
+          ? `${address.line1} ${address.line2}, ${address.city} ${address.state} ${address.postalCode}`
+          : `${address.line1}, ${address.city} ${address.state} ${address.postalCode}`,
+      },
+      { key: 'Phone number', value: phoneNumber },
+      {
+        key: 'Primary policyholder?',
+        value: isPrimary ? 'Yes' : 'No',
+      },
+    ];
+  });
+}
 
 function PolicyHoldersView() {
-  const { isLoading, error, data } = useQuery('repoData', () =>
-    fetch(
-      'https://fe-interview-technical-challenge-api-git-main-sure.vercel.app/api/policyholders'
-    ).then((res) => res.json())
+  const [policyData, setPolicyData] = useState();
+  const [addPolicy, setAddPolicy] = useState(false);
+  const { isLoading, error, data } = useQuery(
+    'repoData',
+    async () =>
+      await axios.get(
+        'https://fe-interview-technical-challenge-api-git-main-sure.vercel.app/api/policyholders'
+      )
   );
+
+  useEffect(() => {
+    if (data) {
+      setPolicyData(data?.data?.policyHolders);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (addPolicy) {
+      axios
+        .post(
+          'https://fe-interview-technical-challenge-api-git-main-sure.vercel.app/api/policyholders',
+          {
+            name: 'Anabelle Policyholder',
+            age: 52,
+            phoneNumber: '1-555-203-2111',
+            isPrimary: false,
+            address: {
+              line1: '123 Fake St',
+              city: 'Nuremburg',
+              state: 'Switzerland',
+              postalCode: '0822112',
+            },
+          }
+        )
+        .then((response) => setPolicyData(response.data.policyHolders));
+    }
+  }, [addPolicy]);
+
+  const handleAddPolicyHolder = () => {
+    setAddPolicy(true);
+  };
 
   if (isLoading) {
     return <p>Loading!</p>;
@@ -19,55 +99,33 @@ function PolicyHoldersView() {
     );
   }
 
-  interface PolicyHolder {
-    name: string;
-    age: string;
-    phoneNumber: string;
-    isPrimary: boolean;
-    address: {
-      line1: string;
-      line2: string;
-      city: string;
-      state: string;
-      postalCode: string;
-    };
-  }
+  let personRows: Row[][] = [];
 
-  interface Row {
-    key: string;
-    value: string;
-  }
-
-  const { policyHolders } = data;
-
-  let personRows: Row[] = [];
-
-  if (policyHolders) {
-    // Supports having multiple policy holders returned by the query.
-    personRows = policyHolders.flatMap((policyHolder: PolicyHolder) => {
-      const { name, age, phoneNumber, isPrimary, address } = policyHolder;
-
-      return [
-        { key: 'Name', value: name },
-        { key: 'Age', value: age },
-        {
-          key: 'Address',
-          value: address.line2
-            ? `${address.line1} ${address.line2}, ${address.city} ${address.state} ${address.postalCode}`
-            : `${address.line1}, ${address.city} ${address.state} ${address.postalCode}`,
-        },
-        { key: 'Phone number', value: phoneNumber },
-        {
-          key: 'Primary policyholder?',
-          value: isPrimary ? 'Yes' : 'No',
-        },
-      ];
-    });
+  if (policyData) {
+    personRows = getPolicyHolderKeys(policyData);
   }
 
   return (
     <Box>
-      <InfoTable header="Policy Holders" rows={personRows}></InfoTable>
+      {personRows.map((row, index) => {
+        return (
+          <InfoTable
+            sx={{ marginBottom: '35px' }}
+            key={index}
+            header="Policy Holder:"
+            rows={row}
+          ></InfoTable>
+        );
+      })}
+
+      <Button
+        variant="contained"
+        color="primary"
+        size="large"
+        onClick={handleAddPolicyHolder}
+      >
+        Add a new policy
+      </Button>
     </Box>
   );
 }
